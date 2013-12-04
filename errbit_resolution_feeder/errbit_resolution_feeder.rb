ecd3d3ff7d6f48fdec5fcca3fd295c23b5e85f85
Martin Naumann <martin.naumann@centralway.com>
2013-12-04 13:17:37 +0100

    Fixed aggregation query (tested it thorougly on actual data)

--- errbit_resolution_feeder/errbit_resolution_feeder.rb
+++ errbit_resolution_feeder/errbit_resolution_feeder.rb
@@ -17,10 +17,18 @@ db = MongoClient.new(opts[:dbHost], opts[:dbPort]).db(opts[:dbName])
 problems = db.collection("problems")
 
 open_problems_number = problems.find({resolved: false}).to_a.count
-problems_resolved_per_month = problems.aggregate([ { "$match" => { resolved: true } }, { "$project" => { month_resolved: { "$month" => "$resolved_at"  } } }, { "$group" => { "_id" => "$month_resolved", number: { "$sum" => 1 } } }, { "$sort" => { "_id" => 1 } }, { "$limit" => 3 } ]).map { |p| p["number"] }
+problems_resolved_per_month = problems.aggregate([
+  { "$match"   => { resolved: true } },
+  { "$project" => { month_resolved: { "$month" => "$resolved_at"  }, message: "$message" }},
+  { "$group"   => { _id: "$month_resolved", problems: { "$addToSet" => '$message' } } },
+  { "$unwind"  =>  "$problems" },
+  { "$group"   => { _id: "$_id", number: { "$sum" => 1 } } },
+  { "$sort"    => { _id: 1 } },
+  { "$limit"   => 3 }
+]).map { |p| p['number'] }
 
 puts "FOUND #{open_problems_number} unresolved problems"
-puts "Resolutions for this month: #{problems_resolved_per_month[2]}"
+puts "Resolutions for this month: #{problems_resolved_per_month[-1]}"
 
 Geckoboard::Push.api_key = opts[:apiKey]
 push = Geckoboard::Push.new(opts[:widgetKey])
